123f88e3f8be3f09ddf19f9eb5d51a7b01bd9538
Bruce Pannaman <Bruce@Bruces-MacBook-Pro.local>
2016-04-24 20:00:46 +0100

    now prints and can deal with multiple parameters

--- Customisation_Templates.py
+++ Customisation_Templates.py
@@ -3,7 +3,7 @@ __author__ = 'brucepannaman'
 import zipfile
 import csv
 import os
-import datetime
+import subprocess
 import time
 import shutil
 
@@ -18,58 +18,55 @@ templateDocx = zipfile.ZipFile(template_name)
 Customisation_list = open(customization_list, "rb")
 reader = csv.reader(Customisation_list)
 
-folder_name = time.strftime('%Y-%m-%d %H:%M:%S')
+folder_name = time.strftime('%Y-%m-%d-%H:%M:%S')
 os.mkdir(folder_name)
 
 letter_iterator = 0
 
 for row in reader:
-    replacement_dict = {}
-    field_iterator = 1
+    replacement_dict = []
 
     with open(templateDocx.extract("word/document.xml", "")) as tempXmlFile:
         tempXmlStr = tempXmlFile.read()
-        num_variables = str.count(tempXmlStr, '{parameter')
-
-
+        num_variables = str.count(tempXmlStr, 'param')
 
     for field in row:
-        replacement_dict["param"] = field
-        field_iterator = field_iterator + 1
+        replacement_dict.append(field)
 
+# Validation of the customization list against the variables
     if len(replacement_dict) > num_variables:
-        print "Holy shit man, you have too many variables in your customisation sheet"
+        print "Holy shit man, there are not enough paramater in your template for the ones ons your customisation sheet"
 
     if len(replacement_dict) < num_variables:
         print "Holy shit man, you have too few variables in your customisation sheet"
 
     else:
-        print "All Good in the hood, creating your letter"
+        print "All Good in the hood, creating your letter with variables %s" % replacement_dict
 
-        # Creates the new letter replacing the headers
-
-        # Open a new word document
-
+# Creates the new letter replacing the headers
         newDocx = zipfile.ZipFile("%s/letter%s.docx" % (folder_name, letter_iterator), "a")
 
-        for key in replacement_dict.keys():
-            print key
-            print replacement_dict.get(key)
-            tempXmlStr = tempXmlStr.replace(str(key), str(replacement_dict.get(key)))
+# Replaces each param with the field in the customization dictionary
+        for key in replacement_dict:
+            tempXmlStr = tempXmlStr.replace("param", str(key), 1)
 
+# Create a tmp file to put the new xml contents in
         with open("temp.xml", "w+") as tempXmlFile:
             tempXmlFile.write(tempXmlStr)
 
+# Write these new xmls to the new file in question
         for file in templateDocx.filelist:
             if not file.filename == "word/document.xml":
                 newDocx.writestr(file.filename, templateDocx.read(file))
 
+# Compress the new word file together
         newDocx.write("temp.xml", "word/document.xml")
 
+# Send it to the printer
+        print "Sending to printer letter with variables %s" % replacement_dict
+        subprocess.check_call(["lp", "-d", "Hannah_s_Printer", "%s/letter%s.docx" % (folder_name, letter_iterator)])
+
         letter_iterator = letter_iterator + 1
-
-
-
         newDocx.close()
 
 shutil.rmtree('word')
