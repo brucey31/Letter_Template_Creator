f4cb326df7fab720a5f06250fc26463ea71c8269
Bruce Pannaman <Bruce@Bruces-MacBook-Pro.local>
2016-12-11 10:22:46 +0000

    better regex around changing units

--- testing_ground.py
+++ testing_ground.py
@@ -1,40 +1,42 @@
 import subprocess
 import os
-
-
+import re
 
 print 'Changing Objects to Paths'
 subprocess.call(["inkscape", "letter24.svg", "--export-text-to-path", "--export-plain-svg", "letter24_2.svg"],
                 cwd="/home/pi/Desktop/")
 
-with open("/home/pi/Desktop/letter24_3.svg", 'w') as write_file:
-    with open('/home/pi/Desktop/letter24_2.svg', 'r') as svg_file:
+print 'Ungrouping all'
+subprocess.call(["inkscape", "-f","letter24_2.svg", "-g", "--verb", "EditSelectAll", "--verb", "SelectionUnGroup",
+                 "--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb",
+                 "SelectionUnGroup","--verb", "SelectionUnGroup",
+                 "--verb", "FileSave", "--verb", "FileQuit"],
+                cwd="/home/pi/Desktop/")
+
+print "Removing Last Line"
+subprocess.call(["inkscape", "-f",  "letter24_2.svg", "-g",  "--verb", "EditSelectAll", "--verb",
+                 "net.wasbo.filter.reopenSingleLineFont.noprefs", "--verb", "FileSave", "--verb", "FileQuit"],
+                cwd="/home/pi/Desktop/")
+
+print "Optimising Document Units for CNC Operations"
+with open("/Users/Bruce/Desktop/letter24_3.svg", 'w+') as write_file:
+    with open('/Users/Bruce/Desktop/letter24_2.svg', 'r') as svg_file:
+        regex1 = 'height=".*"'
+        regex2 = 'width=".*"'
         for line in svg_file:
-            line2 = line.replace('height="1045.275565px"', 'height="297mm"')
-            line3 = line2.replace('width="744.09447px"', 'width="210mm"')
-            write_file.write(line3)
+            line = re.sub(regex1, 'height="297mm"', line)
+            line = re.sub(regex2, 'width="210mm"', line)
+            write_file.write(line)
     svg_file.close()
 write_file.close()
 
-# print 'Ungrouping all'
-# subprocess.call(["inkscape", "-f","letter24_3.svg", "-g", "--verb", "EditSelectAll", "--verb", "SelectionUnGroup",
-#                  "--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb",
-#                  "SelectionUnGroup","--verb", "SelectionUnGroup",
-#                  "--verb", "FileSave", "--verb", "FileQuit"],
-#                 cwd="/home/pi/Desktop/")
-#
-# print "Removing Last Line"
-# subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "EditSelectAll", "--verb",
-#                  "net.wasbo.filter.reopenSingleLineFont.noprefs", "--verb", "FileSave", "--verb", "FileQuit"],
-#                 cwd="/home/pi/Desktop/")
-#
-# # os.remove('/home/pi/Desktop/letter24.svg')
-# os.remove('/home/pi/Desktop/letter24_2.svg')
-#
-# print "Sending file to Axidraw"
-# subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "command.evilmadscientist.axidraw.rev110b.noprefs"
-#                  ,"--verb", "FileSave", "--verb", "FileQuit"],
-#                 cwd="/home/pi/Desktop/")
-#
-# print "Finished Finally"
-#
+# os.remove('/home/pi/Desktop/letter24.svg')
+os.remove('/home/pi/Desktop/letter24_2.svg')
+
+print "Sending file to Axidraw"
+subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "command.evilmadscientist.axidraw.rev110b.noprefs"
+                 ,"--verb", "FileSave", "--verb", "FileQuit"],
+                cwd="/home/pi/Desktop/")
+
+print "Finished Finally"
+
