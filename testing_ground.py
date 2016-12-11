e3cd561fe1b5fad27fe9b1e3ab21ed39d457b15f
Bruce <bruce@pannaman.com>
2016-12-11 10:19:56 +0000

    added axidraw code

--- testing_ground.py
+++ testing_ground.py
@@ -1,8 +1,14 @@
 import subprocess
 import os
 
-with open("/home/pi/Desktop/letter24_2.svg", 'w') as write_file:
-    with open('/home/pi/Desktop/letter24.svg', 'r') as svg_file:
+
+
+print 'Changing Objects to Paths'
+subprocess.call(["inkscape", "letter24.svg", "--export-text-to-path", "--export-plain-svg", "letter24_2.svg"],
+                cwd="/home/pi/Desktop/")
+
+with open("/home/pi/Desktop/letter24_3.svg", 'w') as write_file:
+    with open('/home/pi/Desktop/letter24_2.svg', 'r') as svg_file:
         for line in svg_file:
             line2 = line.replace('height="1045.275565px"', 'height="297mm"')
             line3 = line2.replace('width="744.09447px"', 'width="210mm"')
@@ -10,29 +16,25 @@ with open("/home/pi/Desktop/letter24_2.svg", 'w') as write_file:
     svg_file.close()
 write_file.close()
 
-print 'Changing Objects to Paths'
-subprocess.call(["inkscape", "letter24_2.svg", "--export-text-to-path", "--export-plain-svg", "letter24_3.svg"],
-                cwd="/home/pi/Desktop/")
-
-print 'Ungrouping all'
-subprocess.call(["inkscape", "-f","letter24_3.svg", "-g", "--verb", "EditSelectAll", "--verb", "SelectionUnGroup",
-                 "--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb",
-                 "SelectionUnGroup","--verb", "SelectionUnGroup",
-                 "--verb", "FileSave", "--verb", "FileQuit"],
-                cwd="/home/pi/Desktop/")
-
-print "Removing Last Line"
-subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "EditSelectAll", "--verb",
-                 "net.wasbo.filter.reopenSingleLineFont.noprefs", "--verb", "FileSave", "--verb", "FileQuit"],
-                cwd="/home/pi/Desktop/")
-
-# os.remove('/home/pi/Desktop/letter24.svg')
-os.remove('/home/pi/Desktop/letter24_2.svg')
-
-print "Sending file to Axidraw"
-subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "command.evilmadscientist.axidraw.rev110b.noprefs"
-                 ,"--verb", "FileSave", "--verb", "FileQuit"],
-                cwd="/home/pi/Desktop/")
-
-print "Finished Finally"
-
+# print 'Ungrouping all'
+# subprocess.call(["inkscape", "-f","letter24_3.svg", "-g", "--verb", "EditSelectAll", "--verb", "SelectionUnGroup",
+#                  "--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb",
+#                  "SelectionUnGroup","--verb", "SelectionUnGroup",
+#                  "--verb", "FileSave", "--verb", "FileQuit"],
+#                 cwd="/home/pi/Desktop/")
+#
+# print "Removing Last Line"
+# subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "EditSelectAll", "--verb",
+#                  "net.wasbo.filter.reopenSingleLineFont.noprefs", "--verb", "FileSave", "--verb", "FileQuit"],
+#                 cwd="/home/pi/Desktop/")
+#
+# # os.remove('/home/pi/Desktop/letter24.svg')
+# os.remove('/home/pi/Desktop/letter24_2.svg')
+#
+# print "Sending file to Axidraw"
+# subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "command.evilmadscientist.axidraw.rev110b.noprefs"
+#                  ,"--verb", "FileSave", "--verb", "FileQuit"],
+#                 cwd="/home/pi/Desktop/")
+#
+# print "Finished Finally"
+#
