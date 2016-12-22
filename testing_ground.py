7aa1cf08c9af77bf0bfe40756c9001e94a28b705
Bruce Pannaman <Bruce@Bruces-MacBook-Pro.local>
2016-12-22 21:00:08 +0000

    axidraw now works and file close added

--- testing_ground.py
+++ testing_ground.py
@@ -21,42 +21,42 @@ except Exception as e:
 print 'Changing Objects to Paths'
 try:
     subprocess.call(["inkscape", "letter24_2.svg", "--export-text-to-path", "--export-plain-svg", "letter24_3.svg"],
-                cwd="/home/pi/Desktop/")
+                    cwd="/home/pi/Desktop/")
 except Exception as e:
     print "Failed to change objects to path within the svg"
     print e
 
 print 'Ungrouping all'
 try:
-    subprocess.call(["inkscape", "-f","letter24_3.svg", "-g", "--verb", "EditSelectAll", "--verb", "SelectionUnGroup",
-                 "--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb", "SelectionUnGroup","--verb",
-                 "SelectionUnGroup","--verb", "SelectionUnGroup",
-                 "--verb", "FileSave", "--verb", "FileQuit"],
-                cwd="/home/pi/Desktop/")
+    subprocess.call(["inkscape", "-f", "letter24_3.svg", "-g", "--verb", "EditSelectAll", "--verb", "SelectionUnGroup",
+                     "--verb", "SelectionUnGroup", "--verb", "SelectionUnGroup", "--verb", "SelectionUnGroup", "--verb",
+                     "SelectionUnGroup", "--verb", "SelectionUnGroup",
+                     "--verb", "FileSave", "--verb", "FileQuit"],
+                    cwd="/home/pi/Desktop/")
 except Exception as e:
     print "Failed to ungroup the text into individual letters"
     print e
 
 print "Removing Last Line"
 try:
-    subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "EditSelectAll", "--verb",
-                 "net.wasbo.filter.reopenSingleLineFont.noprefs", "--verb", "FileSave", "--verb", "FileQuit"],
-                cwd="/home/pi/Desktop/")
+    subprocess.call(["inkscape", "-f", "letter24_3.svg", "-g", "--verb", "EditSelectAll", "--verb",
+                     "net.wasbo.filter.reopenSingleLineFont.noprefs", "--verb", "FileSave", "--verb", "FileQuit"],
+                    cwd="/home/pi/Desktop/")
 except Exception as e:
     print "Failed to remove last line from single line font"
     print e
-
 
 # os.remove('/home/pi/Desktop/letter24.svg')
 os.remove('/home/pi/Desktop/letter24_2.svg')
 
 print "Sending file to Axidraw"
 try:
-    subprocess.call(["inkscape", "-f",  "letter24_3.svg", "-g",  "--verb", "command.evilmadscientist.axidraw.rev110b.noprefs"],
-                cwd="/home/pi/Desktop/")
+    subprocess.call(
+        ["inkscape", "-f", "letter24_3.svg", "-g", "--verb", "command.evilmadscientist.axidraw.rev110b.noprefs",
+         "--verb", "FileSave", "--verb", "FileQuit"],
+        cwd="/home/pi/Desktop/")
 except Exception as e:
     print "Failed to send to Axidraw"
     print e
 
 print "Finished Finally"
-
